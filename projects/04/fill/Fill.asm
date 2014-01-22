b16dd41b511fa5d61526be06a2d7dfc0734bc1c6
urthbound <jeffowler@gmail.com>
2014-01-22 12:58:18 -0500

    flashing working

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -2,8 +2,10 @@
 D=A
 @address
 M=D
-
-
+@8192
+D=A
+@8
+M=D
 @0
 D=!A
 @address
@@ -11,5 +13,32 @@ A=M
 M=D
 @address
 M=M+1
-@4
+@8
+M=M-1
+M, JGT
+@18
+1, JEQ
+@SCREEN
+D=A
+@address
+M=D
+@8192
+D=A
+@28
+M=D
+@0
+D=A
+@address
+A=M
+M=D
+@address
+M=M+1
+@28
+M=M-1
+M, JGT
+@KBD
+D=M
+@0
+D, JNE
+@22
 0, JMP
