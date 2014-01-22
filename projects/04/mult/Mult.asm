1dc327aad1be415c1d271cbb99c0451d042bb374
urthbound <jeffowler@gmail.com>
2014-01-22 01:39:32 -0500

    mult works

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -1,20 +1,21 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
-
-// Put your code here.
+@R2
+M=0
+@R1
+D=M
+@20
+D, JEQ
+@R1
+D=M
+@20
+D, JEQ
 @R0
 D=M
 @R2
 M=M+D
-@R0
+@R1
 M=M-1
 D=M
-@0
+@10
 D, JGT
-@16
-0, JMP
+@19
+0; JMP
